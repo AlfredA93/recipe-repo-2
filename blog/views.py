75bec66b45b2eab284b6b299bdbd79233bb25349
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-08-09 11:31:29 +0200

    Edit paginate_by value in views.py

--- blog/views.py
+++ blog/views.py
@@ -9,4 +9,4 @@ class RecipeList(generic.ListView):
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-likes')
     template_name = 'index.html'
-    paginate_by = 18
+    paginate_by = 9
