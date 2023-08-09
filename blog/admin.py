1e9e2ce340eee706994d4e61a3bb357de9b6aeef
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-08-09 09:31:08 +0200

    Register models with admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Recipe, Comment, Bookmark
 
-# Register your models here.
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    "Recipe Admin fields and filtering"
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'season', 'published_on')
+    list_display = ('title', 'status', 'season', 'published_on')
+    search_fields = ['title', 'instructions', 'tags']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    "Comment Admin fields and filtering"
+    list_filter = ['published']
+    list_display = ('user', 'recipe', 'content', 'published')
+    search_fields = ['recipe', 'content']
+    
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    "Bookmark Admin fields and filtering"
+    list_filter = ('recipe', 'bookmark_created')
+    list_display = ('user', 'recipe', 'bookmark_created')
+    search_fields = ['user', 'recipe']
