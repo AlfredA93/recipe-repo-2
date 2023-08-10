33cb6bb05ee7486711927c2795615cea990418dc
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-08-10 13:41:59 +0200

    Add allauth url path and edit login/logout settings.py

--- recipe_repo/settings.py
+++ recipe_repo/settings.py
@@ -53,6 +53,11 @@ INSTALLED_APPS = [
     'cloudinary',
 ]
 
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
