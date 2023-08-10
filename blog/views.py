348d71f19f8f3300a4240c320fcb70b9cbbb8cda
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-08-10 09:38:20 +0200

    Add RecipeDetail View and urlpattern

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 """ Libraries """
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 
@@ -10,3 +10,23 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-likes')
     template_name = 'index.html'
     paginate_by = 9
+
+class RecipeDetail(View):
+    "Full Recipe View, Single Recipe per page"
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1, slug=slug)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.order_by('published')
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "liked": liked
+            },
+        )
