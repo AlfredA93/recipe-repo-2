f3950901a12f30f44734bbfe8400f68cafba72b4
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-08-15 14:10:19 +0200

    Edit widgets to forms.py for CommentForm styling

--- blog/forms.py
+++ blog/forms.py
@@ -3,12 +3,18 @@ from django import forms
 
 
 class CommentForm(forms.ModelForm):
+    "Comment input form"
     class Meta:
+        "Metadata about form"
         model = Comment
         fields = ('content',)
         labels = {
             'content': ''
         }
         widgets = {
-            'content': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Comment'})
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Comment',
+                'rows': '4',
+                'cols': '40'}),
         }
