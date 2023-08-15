b9dc1fdebd370080c9ebb11ffecdc190167bf016
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-08-15 13:06:41 +0200

    Add labels, widgets to form CommentForm for styling

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('content',)
+        labels = {
+            'content': ''
+        }
+        widgets = {
+            'content': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Comment'})
+        }
