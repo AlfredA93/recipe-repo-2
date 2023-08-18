b96cf944b328bea6633977d1b2491517143b8bd3
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-08-18 09:24:14 +0200

    Create url path for my_recipes.html - urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
     path('bookmarks/', views.BookmarkList.as_view(), name='bookmarks_list'),
     path('add_recipe/', views.AddRecipe.as_view(), name='add_recipe'),
+    path('my_recipes/', views.MyRecipeList.as_view(), name='my_recipes'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
     path('like/<slug:slug>/', views.RecipeLike.as_view(), name='recipe_like'),
     path('save/<slug:slug>/', views.RecipeBookmark.as_view(),
