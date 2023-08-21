bbdc05ffda6e0e1bec5185f57b2f48d4dd1d6484
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-08-21 15:07:35 +0200

    Edit RecipeAdmin view - admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,14 @@
+"""Admin page to display data as registered in this file"""
 from django.contrib import admin
 from .models import Recipe, Comment
+
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     "Recipe Admin fields and filtering"
     prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('status', 'season', 'published_on')
-    list_display = ('title', 'status', 'season', 'published_on')
+    list_filter = ('status', 'season', 'author', 'published_on')
+    list_display = ('title', 'author', 'status', 'season', 'published_on')
     search_fields = ['title', 'instructions', 'tags']
 
 
