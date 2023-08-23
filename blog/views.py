a39098a200aa526f909e58a26c544a917303d74a
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-08-23 14:32:52 +0200

    Edit Paginate_by on RecipeList view - views.py

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ class RecipeList(generic.ListView):
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-likes')
     template_name = 'index.html'
-    paginate_by = 9
+    paginate_by = 12
     ordering = ['published_on']
 
 
