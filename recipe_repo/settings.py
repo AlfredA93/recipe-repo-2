9e71b2d237fecd8472bbf5fe6fdc675351a0d326
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-08-25 13:16:24 +0200

    Comment out Static files Directory - settings

--- recipe_repo/settings.py
+++ recipe_repo/settings.py
@@ -171,7 +171,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
