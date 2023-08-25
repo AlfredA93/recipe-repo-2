f0506cda7c02e5b453d4d025166da30ac1410f59
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-08-25 13:23:19 +0200

    Add django_heroku to settings.py

--- recipe_repo/settings.py
+++ recipe_repo/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import django_heroku
 from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
      import env
@@ -181,3 +182,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
