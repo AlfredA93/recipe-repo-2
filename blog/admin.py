2dc9b1ce82ce933a36c7d3552dd3cdfdd1069980
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-08-25 15:28:36 +0200

    Delete Summernote - admin, urls.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,16 +1,14 @@
 """Admin page to display data as registered in this file"""
 from django.contrib import admin
-from django_summernote.admin import SummernoteModelAdmin
 from .models import Recipe, Comment
 
 
 @admin.register(Recipe)
-class RecipeAdmin(SummernoteModelAdmin):
+class RecipeAdmin(admin.ModelAdmin):
     "Recipe Admin fields and filtering"
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'season', 'author', 'published_on')
     list_display = ('title', 'author', 'status', 'season', 'published_on')
-    summernote_fields = ('zutaten', 'directions')
 
 
 @admin.register(Comment)
