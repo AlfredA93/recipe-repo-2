3837bfd77efd53a854762f4f0076e6d2d3ebec86
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-08-25 14:21:08 +0200

    Edit admin summerModelAdmin, add sumernote url

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,16 @@
 """Admin page to display data as registered in this file"""
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Recipe, Comment
 
 
 @admin.register(Recipe)
-class RecipeAdmin(admin.ModelAdmin):
+class RecipeAdmin(SummernoteModelAdmin):
     "Recipe Admin fields and filtering"
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'season', 'author', 'published_on')
     list_display = ('title', 'author', 'status', 'season', 'published_on')
+    summernote_fields = ('zutaten', 'directions')
 
 
 @admin.register(Comment)
