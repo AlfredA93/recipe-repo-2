825a543d98c20950c3e193769496ead4b05916bb
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-08-25 13:28:09 +0200

    Edit static files - settings.py

--- recipe_repo/settings.py
+++ recipe_repo/settings.py
@@ -171,8 +171,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
